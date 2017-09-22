e2a914ccdbf6f7be9e0c365cb575f9fbdafab648
Jared Price <jaredprice25@gmail.com>
2017-09-21 20:37:14 -0600

    working server

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -1,7 +1,7 @@
 let books = [];
 let id = 0;
 
-mode.exports = {
+module.exports = {
     create: (req, res) => {
         const { title, author } = req.body;
         books.push( { id, title, author } );
@@ -16,5 +16,21 @@ mode.exports = {
     update: (req, res ) => {
         const updateID = req.params.id;
         let index = books.findIndex( book => book.id == updateID );
+
+        books[ index ] = {
+            id: books[ index ].id,
+            title: req.body.title || books[ index ].title,
+            author: req.body.author || books[ index ].author
+        };
+
+        res.status(200).send( books );
+    },
+
+    delete: (req, res ) => {
+        const deleteID = req.params.id;
+        bookID = books.findIndex( book => book.id == deleteID);
+        books.splice( bookID, 1 );
+        res.status(200).send( books );
     }
-}
+};
+
--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bc = require('./controllers/books_controller.js');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+const baseURL = "/api/books";
+app.post(baseURL, bc.create);
+app.get(baseURL, bc.read);
+app.put(`${baseURL}/:id`, bc.update);
+app.delete(`${baseURL}/:id`, bc.delete);
+
 const port = 3000;
-app.listen(port, () => {`Server listening on port ${port}`); } );
+app.listen(port, () => { console.log(`Server listening on port ${port}`) } );
